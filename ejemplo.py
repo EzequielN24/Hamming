9d6536fe396dc38676add6b0dfe3290c3b598505
Ezequiel <ezequielnodarhbk@gmail.com>
2025-04-10 15:33:10 -0300

    funcion de hamming

--- ejemplo.py
+++ ejemplo.py
@@ -20,34 +20,25 @@ def colocar_bit_control(p):
     aux2= aux2 | (( ((aux2 & 16) >> 4) ^ ((aux2 & 8) >> 3) ^ ((aux2 & 4) >> 2) ^ ((aux2 & 2) >> 1) ) << 4)
     aux2= aux2 | (( ((aux2 & 128) >> 7) ^ ((aux2 & 64) >> 6) ^ ((aux2 & 32) >> 5) ^ ((aux2 & 16) >> 4) ^ ((aux2 & 8) >> 3) ^ ((aux2 & 4) >> 2) ^ ((aux2 & 2) >> 1) ))
     print(bin(aux2))
-    print(aux1)
-    print(aux2)
 
 
           
 
-def jaja(p):
-    primer=(int((ord(p))))
-    primer >>=4
-    control1=(primer & 8) 
-    control1=(((primer & 8) >> 3) + ((primer & 4) >> 2)+ ((primer & 1))) % 2
-    control2=(((primer & 8) >> 3) + ((primer & 2) >> 1)+ ((primer & 1))) % 2
-    control3=(((primer & 4) >> 2)+ ((primer & 2) >> 1) + ((primer & 1))) % 2
-    paridad=(control1+ control2 + control3 + (((primer & 8) >> 3) + ((primer & 4) >> 2)+ ((primer & 1)) + ((primer & 2) >> 1)))%2
+def hamminizacion(p):
+    primer=codificacion_hamming(int((ord(p)))>>4)
+    segundo=codificacion_hamming(int((ord(p))% 16))
+    return {"primer" : primer , "segundo" : segundo}
+
+
+
+def codificacion_hamming(p):
+    control1=(((p & 8) >> 3) + ((p & 4) >> 2)+ ((p & 1))) % 2
+    control2=(((p & 8) >> 3) + ((p & 2) >> 1)+ ((p & 1))) % 2
+    control3=(((p & 4) >> 2)+ ((p & 2) >> 1) + ((p & 1))) % 2
+    paridad=(control1+ control2 + control3 + (((p & 8) >> 3) + ((p & 4) >> 2)+ ((p & 1)) + ((p & 2) >> 1)))%2
     num=0
-    num = (control1 << 7) + (control2 << 6) + ((primer & 8) << 2) + (control3 << 4) + ((primer & 4) << 1) + ((primer & 2) << 1) + ((primer & 1) << 1) + paridad
-    print(bin(num));    
-
-
-    segundo=int((ord(p))% 16)
-    control1=(((segundo & 8) >> 3) + ((segundo & 4) >> 2)+ ((segundo & 1))) % 2
-    control2=(((segundo & 8) >> 3) + ((segundo & 2) >> 1)+ ((segundo & 1))) % 2
-    control3=(((segundo & 4) >> 2)+ ((segundo & 2) >> 1) + ((segundo & 1))) % 2
-    paridad=(control1+ control2 + control3 + (((segundo & 8) >> 3) + ((segundo & 4) >> 2)+ ((segundo & 1)) + ((segundo & 2) >> 1)))%2
-    num=0
-    num = (control1 << 7) + (control2 << 6) + ((segundo & 8) << 2) + (control3 << 4) + ((segundo & 4) << 1) + ((segundo & 2) << 1) + ((segundo & 1) << 1) + paridad
-    print(bin(num));
-
+    num = (control1 << 7) + (control2 << 6) + ((p & 8) << 2) + (control3 << 4) + ((p & 4) << 1) + ((p & 2) << 1) + ((p & 1) << 1) + paridad
+    return num
 
 
 """
