36fe9423610d606909a3d4d2449c941579993e52
EzeBernaldez <ezebernaldez@gmail.com>
2025-04-24 21:44:28 -0300

    hamming de 4096 hecho

---
+++ hamming_4096.py
@@ -0,0 +1,173 @@
+import random
+
+def crear_numero_4096(p):
+    # p contiene 240 bits de datos
+    j = 0
+    res = 0
+    for i in range(1, 4097):
+        if (i & (i - 1)) == 0:  # Potencia de 2: posición de bit de control
+            continue
+        elif j < 4080:
+            bit = (p >> (4079 - j)) & 1  # Solo tomamos 240 bits
+            res |= (bit << (4096 - i))
+            j += 1
+        else:
+            # Rellenamos con ceros del 241 al 247 (se agregan automáticamente al mantener res en 0)
+            continue
+    return codificacion_hamming_4096(res)
+
+
+def codificacion_hamming_4096(p):
+    for bit_pos in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]:
+        control = calcular_bit_control(p, bit_pos)
+        p |= (control << (4096 - bit_pos))
+    p=calcular_bit_paridad(p)
+    return p
+
+
+
+def calcular_bit_control(p:int, pos:int) -> int:
+    control = 0
+    for i in range(pos, 4096, pos * 2):
+        for j in range(i, i + pos):
+            control ^= (p >> (4096 - j)) & 1
+    return control
+
+
+
+def calcular_bit_paridad(p):
+    paridad=0
+    for i in range (1,4096):
+        paridad ^= (p >> (4096 - i)) & 1
+    p^= paridad
+    return p
+
+
+
+def codificar_archivo_4096(file_name_read, file_name_write):
+    try:
+        with open(file_name_read, "rb") as f, open(file_name_write, "wb") as wr:
+            while True:
+                bloque = f.read(510)
+                if len(bloque) == 0:
+                    break
+                valor = int.from_bytes(bloque, byteorder='big')
+                valor <<= (8 * (510 - len(bloque)))
+                num = crear_numero_4096(valor)
+                wr.write(num.to_bytes(512,byteorder='big'))
+    except FileNotFoundError as e:
+        print("Ocurrió un error al abrir los archivos: ", e)
+    except Exception as e:
+        print("Error: ", e)
+
+
+
+
+def deshamminizacion_4096(p,fix_module):
+    decodificacion = control_hamming_4096(p)
+    num =  (decodificacion["s11"] << 11) + (decodificacion["s10"] << 10) + (decodificacion["s9"] << 9) + (decodificacion["s8"] << 8) + (decodificacion["s7"] << 7) + (decodificacion["s6"] << 6) + (decodificacion["s5"] << 5) + (decodificacion["s4"] << 4) + (decodificacion["s3"] << 3) +(decodificacion["s2"] << 2) + (decodificacion["s1"] << 1) + (decodificacion["s0"] )
+    if decodificacion["paridad"] == 0:
+        if num==0:
+            return decodificacion_hamming_4096(p)
+        else:
+            return -1 #Hay dos errores
+    else:
+        if num==0:
+            #Hay error en el bit de paridad
+            if fix_module == 1:
+                p = corregir_error_4096(p,4095) 
+                return decodificacion_hamming_4096(p)
+            else:
+                return decodificacion_hamming_4096(p)
+        else:
+            #Hay error en una posición que no es el bit de paridad
+            if fix_module == 1:
+                p = corregir_error_4096(p,num) 
+                return decodificacion_hamming_4096(p)
+            else:
+                return decodificacion_hamming_4096(p)
+
+
+
+def corregir_error_4096(p,error):
+    return (p ^ (2**4096 >> (error))) 
+
+
+def control_hamming_4096(p):
+    res={}
+    for i,bit_pos in enumerate([1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]):
+        control = calcular_bit_control_deshamminizacion(p, bit_pos)
+        res[f"s{i}"]=control
+    paridad=0
+    for i in range (1,4097):
+        paridad ^= (p >> (4096 - i)) & 1
+    res[f"paridad"]=paridad
+    return res
+
+
+
+def calcular_bit_control_deshamminizacion(p:int, pos:int) -> int:
+    control = 0
+    for i in range(pos, 4096, pos * 2):
+        for j in range(i, i + pos):
+            control ^= ((p >> (4096 - j)) & 1)
+    return control
+
+
+def decodificacion_hamming_4096(p):
+    j = 0
+    res = 0
+    for i in range(1, 4097):
+        if (i & (i - 1)) == 0:
+            continue
+        if j >= 4080:
+            break
+        bit = (p >> (4096 - i)) & 1
+        res |= (bit << (4079 - j))
+        j += 1
+    return res
+
+
+def decodificar_archivo_4096(file_name_read, file_name_write, arreglar_archivo):
+    try:
+        with open(file_name_read, "rb") as f, open(file_name_write, "w",encoding="utf-8") as wr:
+            while True:
+                bloque = f.read(512)
+                bloque_bytes = int.from_bytes(bloque,byteorder="big")
+                if len(bloque) == 0:
+                    break
+                num = deshamminizacion_4096(bloque_bytes,arreglar_archivo)
+                for i in range(510):
+                    shift = 4072 - (8 * (i))
+                    letra = (num >> shift) & 0xFF
+                    if(letra != 0):
+                        if(chr(letra) == "\n"):
+                            pass
+                        else:
+                            wr.write(f"{chr(letra)}")
+    except FileNotFoundError as e:
+        print("Ocurrió un error al abrir los archivos: ", e)
+    except Exception as e:
+        print("Error: ", e)
+
+
+
+'''----------------------------------------------------------------------------------------------------------------'''
+
+def ingresar_error_4096(file_name_read,file_name_write):
+    try:
+        with open(file_name_read, 'rb') as f, open(file_name_write, 'wb') as wr:
+            while True:
+                bloque = f.read(512)
+                bloque_bytes = int.from_bytes(bloque,byteorder='big')
+                if(len(bloque)==0):
+                    break
+                if random.randint(0,1) == 1:
+                    error = random.randint(0,4095)
+                    mask = 1 << error
+                    bloque_bytes^= mask
+                wr.write(bloque_bytes.to_bytes(512,byteorder='big'))
+        
+    except Exception as e:
+        print(f"Error al ingresar error: {e}")
+
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import hamming_256 as ejemplo
+import hamming_4096 as ejemplo
 
 import time
 
@@ -7,13 +7,13 @@ inicio = time.time()
 arreglar_archivo = 0 #1 si se quiere arreglar. 0 en caso contrario
 
 
-ejemplo.codificar_archivo_256('texto_plano.txt','texto_hammificado.txt')
+ejemplo.codificar_archivo_4096('texto_plano.txt','texto_hammificado.txt')
 
-ejemplo.decodificar_archivo_256('texto_hammificado.txt','texto_decodificado.txt',1)
+ejemplo.decodificar_archivo_4096('texto_hammificado.txt','texto_decodificado.txt',1)
 
-ejemplo.ingresar_error_256('texto_hammificado.txt','texto_error.txt')
+ejemplo.ingresar_error_4096('texto_hammificado.txt','texto_error.txt')
 
-ejemplo.decodificar_archivo_256('texto_error.txt','texto_decodificado_error.txt',1)
+ejemplo.decodificar_archivo_4096('texto_error.txt','texto_decodificado_error.txt',0)
 
 fin = time.time()
 
